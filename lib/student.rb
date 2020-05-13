b61df9f46a33db146c562c04e067d12379d417d5
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-13 13:19:07 -0400

    lab complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
-  end
-
-  def self.create_from_collection(students_array)
-    
+    student_hash.each { |key, val| send("#{key}=", val) }
+    @@all << self
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each { |key, val| send("#{key}=", val) }
+  end
+
+  def self.create_from_collection(students_array)
+    students_array.each { |student| new(student) }
   end
 
   def self.all
-    
+    @@all
   end
 end
-

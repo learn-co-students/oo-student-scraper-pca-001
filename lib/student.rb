c98a5bb5d91fe656d9544419f3529108b59ba4b1
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-14 13:15:35 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |hash|
-      Student.new(hash)
-    end
+    students_array.each { |hash| Student.new(hash) }
   end
 
   def add_student_attributes(attributes_hash)
